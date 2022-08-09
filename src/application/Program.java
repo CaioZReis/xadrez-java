ad0ef22f398d46b10389f9778ea3d9ef9b52c54a
Caio Zanchetta Reis <caionohamachi@gmail.com>
2022-08-09 20:17:30 -0300

    Fiz - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,7 +36,13 @@ public class Program {
 				}
 				if(chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotiom (B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						UI.clearScreen();
+						UI.printMatch(chessMatch, captured);
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 			} catch (ChessException e) {
